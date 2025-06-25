0ae3111e8271ccde8c55e1b9aa4dc50c0daa5008
VinayAnupNair <vinayanupnair007@gmail.com>
2025-06-24 23:44:49 -0500

    added interactive main

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import click
 from cli import cli
 from db import init_db
 
+def run_interactive():
+    print("Welcome to the project manager, type 'quit' or 'exit' to exit the program")
+    while True:
+        try:
+            cmd = input("pm >>> ")
+            if cmd.lower() in {'quit','exit'}:
+                break
+            if cmd:
+                cli.main(args=cmd.split(), standalone_mode=False)
+        except Exception as e:
+            print("error occured : {e}")    
+
 if __name__ == "__main__":
     init_db()
-    cli()
+    run_interactive()
